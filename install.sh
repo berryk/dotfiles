60ad72d0c4da5c105fcfa9f4125c40c6e65edb67
Keith Berry <keithwberry@gmail.com>
2018-03-23 13:57:25 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+dotfiles_dir=~/dotfiles
+
+#==============
+# Delete existing dot files and folders
+#==============
+
+sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.bash_logout > /dev/null 2>&1
+sudo rm -rf ~/.profile > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+
+#==============
+# Create symlinks in the home folder
+# Allow overriding with files of matching names in the custom-configs dir
+#==============
+
+ln -sf $dotfiles_dir/bashrc ~/.bashrc
+ln -sf $dotfiles_dir/bash_logout ~/.bash_logout
+ln -sf $dotfiles_dir/profile ~/.profile
+ln -sf $dotfiles_dir/gitconfig ~/.gitconfig
+
