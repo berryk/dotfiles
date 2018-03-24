a390f666a5f7d87a68115ad75fa9680e29c96789
Keith Berry <keithwberry@gmail.com>
2018-03-23 23:52:39 -0400

    Added ssh config

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dotfiles_dir=~/dotfiles
+dropbox_dir=~/dropbox
 
 #==============
 # Delete existing dot files and folders
@@ -11,6 +12,7 @@ sudo rm -rf ~/.bash_logout > /dev/null 2>&1
 sudo rm -rf ~/.profile > /dev/null 2>&1
 sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -Rrf ~/.ssh > /dev/null 2>&1
 
 #==============
 # Create symlinks in the home folder
@@ -22,4 +24,5 @@ ln -sf $dotfiles_dir/bash_logout ~/.bash_logout
 ln -sf $dotfiles_dir/profile ~/.profile
 ln -sf $dotfiles_dir/gitconfig ~/.gitconfig
 ln -sf $dotfiles_dir/tmux.conf ~/.tmux.conf
+ln -sf $dropbox_dir/ssh ~/.ssh
 
